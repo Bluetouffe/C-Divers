f22a754d0e3e6505e759f33105bb3c8a15554a7a
Louis <louisbonicel@gmail.com>
2015-06-12 15:53:51 +0200

    Update main.c

--- DM-Florian/main.c
+++ DM-Florian/main.c
@@ -4,21 +4,7 @@
 #include <string.h>
 #include <math.h>
 
-/*Le SHA-1 prend un message d'un maximum de 2^64 bits en entrée.
-Quatre fonctions booléennes sont définies, elles prennent 3 mots de 32 bits
-en entrée et calculent un mot de 32 bits. Une fonction spécifique de rotation
-est également disponible, elle permet de déplacer les bits vers la gauche
-(le mouvement est circulaire et les bits reviennent à droite).
-Une de ces rotations n'était pas présente dans le SHA-0, elle permet
-de casser certaines caractéristiques linéaires dans la structure.
-Cela permet d'éviter une attaque sur les bits neutres décrite par Eli Biham,
-technique reprise pour calculer la collision complète sur SHA-0 (Antoine Joux et al.).
-
-Le SHA-1 commence par ajouter à la fin du message un bit à 1 suivi d'une série de bits à 0,
-puis la longueur du message initial (en bits) codée sur 64 bits. La série de 0 a une longueur
-telle que la séquence ainsi prolongée a une longueur multiple de 512 bits. L'algorithme
-travaille ensuite successivement sur des blocs de 512 bits.
-Pour chaque bloc l'algorithme calcule 80 tours (ou rondes, « rounds » en anglais) successifs
+/*Pour chaque bloc l'algorithme calcule 80 tours (ou rondes, « rounds » en anglais) successifs
 et applique une série de transformations sur l'entrée. La première étape consiste à calculer
 80 valeurs sur 32 bits. Les 16 premières valeurs sont obtenues directement à partir du bloc
 « message » en entrée. Les 64 autres sont calculées successivement. Le SHA-1 les obtient grâce
@@ -38,6 +24,24 @@ sa méthode de calcul tous les 20 tours et utilise les sorties des tours précé
 blocs ont été traités, les cinq variables concaténées (5 × 32 = 160 bits) représentent la signature.
 */
 
+uint32_t const H0 = 0x67452301;
+uint32_t const H1 = 0xEFCDAB89;
+uint32_t const H2 = 0x98BADCFE;
+uint32_t const H3 = 0x10325476;
+uint32_t const H4 = 0xC3D2E1F0;
+
+uint32_t const K1 = 0x5A827999;
+uint32_t const K2 = 0x6ED9EBA1;
+uint32_t const K3 = 0x8F1BBCDC;
+uint32_t const K4 = 0xCA62C1D6;
+
+typedef struct hashValue{
+    uint32_t h0;
+    uint32_t h1;
+    uint32_t h2;
+    uint32_t h3;
+    uint32_t h4;
+}hashValue;
 
 void charArrayToInt ( char * s_array , uint8_t * i_array )
 {
@@ -100,23 +104,39 @@ uint8_t * preprocessing ( char * message , uint64_t * size)
 
 void hashSHA1 ( char * message )
 {
-	unsigned long int i = 0;
-    short int j = 0;
+    uint8_t j = 0;
     uint8_t k = 0;
-    uint64_t ind = 0;
     uint8_t dec = 0;
 
-    uint64_t size = strlen ( message );
-    printf("Size : %X\n", size);
-    uint8_t * test = ( uint8_t * ) calloc(  size , sizeof( uint8_t ) );
+    uint32_t a = 0;
+    uint32_t b = 0;
+    uint32_t c = 0;
+    uint32_t d = 0;
+    uint32_t e = 0;
+    uint32_t f = 0;
+    uint32_t kt = 0;
+    uint32_t bufferROTL = 0;
+    uint32_t bufferCondensate = 0;
 
-    uint64_t zerosToAdd = ( ( 448 - ( size * 8 ) ) % 512 ) / 8;
+	uint64_t i = 0;
+    uint64_t ind = 0;
+    uint64_t size = 0;
+    uint64_t zerosToAdd = 0;
+    uint64_t numberOfBlocks = 0;
+
+    uint8_t * test = NULL;
+    uint32_t W[80];
+    uint64_t ** divided = NULL;
+
+    size = strlen ( message );
+    test = ( uint8_t * ) calloc(  size , sizeof( uint8_t ) );
+
+    zerosToAdd = ( ( 448 - ( size * 8 ) ) % 512 ) / 8;
     size = ceil( ( (float)(size + zerosToAdd) / 64.0) ) * 64;
-    printf("Size : %d\n", size);
 
-    uint64_t numberOfBlocks = size >> 6;//Divide by 64
+    numberOfBlocks = size >> 6;//Divide by 64
 
-    uint64_t ** divided = (uint64_t**) malloc( numberOfBlocks * sizeof(uint64_t*) );
+    divided = (uint64_t**) malloc( numberOfBlocks * sizeof(uint64_t*) );
 
 	for (i = 0 ; i < numberOfBlocks ; i++ )
 	{
@@ -124,14 +144,11 @@ void hashSHA1 ( char * message )
 	}
 
     test = preprocessing( message , &size);
-    displayArray(size, test);
 
     for ( i = 0 ; i < numberOfBlocks ; i++ )
     {
         for ( j = 0 ; j < 16 ; j++ )
         {
-            i = i;
-            j = j;
             divided[i][j] = 0;
             for ( k = 0 ; k < 4 ; k++ )
             {
@@ -139,22 +156,96 @@ void hashSHA1 ( char * message )
                 dec = 8 * ( 3 - k );
                 divided[i][j] |=  (uint32_t) test[ind] << dec;
             }
-            printf("%X\n",  divided[i][j]);
         }
     }
 
-//    uint32_t h0 = 0x67452301;
-//    uint32_t h1 = 0xEFCDAB89;
-//    uint32_t h2 = 0x98BADCFE;
-//    uint32_t h3 = 0x10325476;
-//    uint32_t h4 = 0xC3D2E1F0;
+
+    hashValue hashValues;
+
+    hashValues.h0 = H0;
+    hashValues.h1 = H1;
+    hashValues.h2 = H2;
+    hashValues.h3 = H3;
+    hashValues.h4 = H4;
+
+    displayArray(size, test);
+
+    for ( i = 0 ; i < numberOfBlocks ; i++ )
+    {
+        //W initialization
+        for ( j = 0 ; j < 80 ; j++ )
+        {
+            if ( j < 16)
+            {
+                W[j] = divided[i][j];
+            }
+            else
+            {
+                bufferROTL = ( ( ( W[j-3] ^ W[j-8] ) ^ W[j-14] ) ^ W[j-16] );
+                W[j] = ( bufferROTL << 1 ) | ( bufferROTL >> ( 32 - 1 ) );
+                printf("W%d : %x\n", j , W[j]);
+            }
+        }
+        //End W initialization
+
+        a = hashValues.h0;
+        b = hashValues.h1;
+        c = hashValues.h2;
+        d = hashValues.h3;
+        e = hashValues.h4;
+
+
+        for ( j = 0 ; j < 80 ; j++ )
+        {
+            if ( j < 20)
+            {
+                f = ( ( b & c ) | ( ~(b) & d ) );
+                kt = K1;
+            }
+            if ( ( j > 19 ) && ( j < 40 ) )
+            {
+                f = ( ( b ^ c ) ^ d );
+                kt = K2;
+            }
+            if ( ( j > 39 ) && ( j < 60 ) )
+            {
+                f = ( ( b & c ) | ( b & d ) ) | ( c & d );
+                kt = K3;
+            }
+            else
+            {
+                f = ( ( b ^ c ) ^ d );
+                kt = K4;
+            }
+
+            bufferROTL = ( a << 5 ) | ( a >> (32 - 5) );
+
+            bufferCondensate = bufferROTL + f + e + kt + W[j];
+
+            e = d;
+            d = c;
+            bufferROTL = ( b << 30 ) | ( b >> (32 - 30) );
+            c = bufferROTL;
+            b = a;
+            a = bufferCondensate;
+
+            hashValues.h0 += a;
+            hashValues.h1 += b;
+            hashValues.h2 += c;
+            hashValues.h3 += d;
+            hashValues.h4 += e;
+        }
+    }
+
+    printf("Resulting hash is :\n%X\n%X\n%X\n%X\n%X\n\n", hashValues.h0 , hashValues.h1 , hashValues.h2 , hashValues.h3 , hashValues.h4 );
+
     free(test);
     free(divided);
 }
 
 int main()
 {
-    char * message = "0123456789@ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    char * message = "";
 //    char * message = "abcdefghijklmnopqrstuvwxyz";
     hashSHA1( message );
 
